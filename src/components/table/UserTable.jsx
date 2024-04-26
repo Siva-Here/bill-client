96caa4eb417485c9b94ac40c86d78a8401f54ee5
siva <sivahere9484@gmail.com>
2024-04-26 21:44:09 +0530

    count uopdated

--- src/components/table/UserTable.jsx
+++ src/components/table/UserTable.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './UserTable.css';
 import axios from 'axios';
+import { PieChart } from 'react-minimal-pie-chart';
+// import ReactPieChart from "chartpiejs";
+import {Pie} from 'react-chartjs-2';
+const dataSets = [
+  {data:[100,200,400],
+  backgroundColor: ['red','green','blue']}
+];
 
 function UserTable() {
   const [text, setText] = useState('');
@@ -8,11 +15,33 @@ function UserTable() {
   const [data, setData] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null); // State to store the selected image URL
   let statusColor = '';
+  let pending = 0;
+  let accepted = 0;
+  let rejected = 0;
+  let pendingBill = 0;
+  let acceptedBill = 0;
+  let rejectedBill = 0;
 
   function handleTextChange(e) {
     setText(e.target.value);
     console.log(text);
   }
+
+  function calculateBills(){
+    data.forEach((data1)=>{
+      if(data1.status=="pending"){
+        pending+=1;
+        pendingBill+=data1.amount; 
+      }else if(data1.status=="accepted"){
+        accepted+=1;
+        acceptedBill+=data1.amount; 
+      }else if(data1.status=="rejected"){
+        rejected+=1;
+        rejectedBill+=data1.amount; 
+      }
+    })
+  }
+  calculateBills();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,15 +80,38 @@ function UserTable() {
         <h1 className='text-white ms-auto text-center flex-1'>Your Bills</h1>
         <br /><hr />
         <div className="d-flex container-sm">
-          <form class="w-50 ms-auto me-auto mb-4" role="search">
+<form class="w-50 ms-auto me-auto mb-4" role="search">
               <input onChange={(e)=>{handleTextChange(e)}} className="p-2 fw-bold form-control me-2" type="search" placeholder="Search Bills" aria-label="Search" />
-          </form>
+          </form>          
           <select className='w-25 ms-auto me-auto mb-4 form-select flex-2' onChange={(e) => { setStatus(e.target.value); }}>
             <option value="All Bills">All Bills</option>
             <option value="pending">pending</option>
             <option value="accepted">accepted</option>
             <option value="rejected">rejected</option>
           </select>
+        </div>
+      </div>
+      <div className="ms-auto me-auto container-md d-flex justify-content-evenly row">        
+      {/* <PieChart
+        data={[
+          { title: 'One', value: 10, color: '#E38627' },
+          { title: 'Two', value: 15, color: '#C13C37' },
+          { title: 'Three', value: 20, color: '#6A2135' },
+        ]}
+      />; */}
+        {/* <ReactPieChart dataSets={dataSets}/> */}
+        {/* <Pie data={{datasets: dataSets}} height='50%'/> */}
+        <div className='upload-outer-div col-10 col-md-4'>
+          <p className="fs-4 fw-bold text-secondary">Pending Bills: {pending}</p>
+          <p className="fs-4 fw-bold text-success">Accepted Bills: {accepted}</p>
+          <p className="fs-4 fw-bold text-danger">Rejected Bills: {rejected}</p>
+        </div>
+        <div className='upload-outer-div col-md-4 col-10'>
+        <div className='flex-1 ms-atuo'>
+          <p className="fs-4 fw-bold text-secondary">Pending Amount: {pendingBill}</p>
+          <p className="fs-4 fw-bold text-success">Accepted Amount: {acceptedBill}</p>
+          <p className="fs-4 fw-bold text-danger">Rejected Amount: {rejectedBill}</p>
+        </div>
         </div>
       </div>
       <table className='table-striped table-hover w-75 container mb-5'>
@@ -73,16 +125,24 @@ function UserTable() {
         <hr id='hr1' />
         <tbody>
           {data.map((data1, index) => {
+            if(data1.status=='pending'){
+              statusColor = 'yellow';
+            }else if(data1.status=='accepted'){
+              statusColor = 'green';
+            }else if(data1.status=='rejected'){
+              statusColor = 'rgb(255,0,0)';
+            }
             return (
               <>
                 {((data1.status === status || status==='All Bills') && (data1.name.toLowerCase().includes(text.toLowerCase()) || data1.type.toLowerCase().includes(text.toLowerCase()))) ? (
                   <>
+                    
                     <tr className='row' key={index}>
                       <td data-cell='SNo' className='col-1 text-center d-none d-md-inline d-sm-none'>{index + 1}</td>
                       <td data-cell='Name' className='col-xs-8 col-sm-4 col-md-2 text-center'>{data1.name}</td>
                       <td data-cell='Amount' className='col-xs-8 col-sm-4 col-md-3 text-center'>{data1.amount}</td>
                       <td data-cell='Type' className='col-xs-8 col-sm-4 col-md-3 text-center'>{data1.type}</td>
-                      <td data-cell='Status' className='col-xs-8 col-sm-4 col-md-3 text-center d-sm-inline' style={{color: {statusColor}}}>{data1.status}</td>
+                      <td data-cell='Status' className={`col-xs-8 col-sm-4 col-md-3 text-center d-sm-inline ${statusColor}`} style={{color: statusColor}}>{data1.status}</td>
                       <td data-cell='image' className='col-12 text-center d-sm-inline'>
                         <img 
                           className='ms-auto me-auto' 

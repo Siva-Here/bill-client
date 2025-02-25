55f82f1b595f6b50ac74817d920308f7b3738c13
yash-166 <n200166@rguktn.ac.in>
2025-02-25 23:54:27 +0530

    small changes in upload component in bill-server

--- src/components/login/MainLog.jsx
+++ src/components/login/MainLog.jsx
@@ -5,7 +5,7 @@ const MainLog = () => {
     return (
         <div className='container'>
             <div className='d-flex vh-100 justify-content-center align-items-center'>
-                <div className="col-lg-5 col-md-6 col-sm-12 mt-3 pt-4 mx-auto">
+                <div className="col-lg-5 col-md-6 col-sm-12  mx-auto">
                     <Log/>
                 </div>
             </div>
